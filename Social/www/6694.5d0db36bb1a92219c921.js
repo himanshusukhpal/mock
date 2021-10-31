2fb53eac705864b8c84e658100f92dff376911b2
Himanshu Sukhpal <himanshu.sukhpal@gmail.com>
2021-10-31 23:18:11 +0530

    removing www

--- Social/www/6694.5d0db36bb1a92219c921.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunksocial=self.webpackChunksocial||[]).push([[6694],{6694:(s,o,c)=>{"use strict";c.r(o),c.d(o,{ion_text:()=>e});var r=c(2884),t=c(6812),l=c(5425);const e=class{constructor(s){(0,r.r)(this,s)}render(){const s=(0,t.b)(this);return(0,r.i)(r.H,{class:(0,l.c)(this.color,{[s]:!0})},(0,r.i)("slot",null))}};e.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
