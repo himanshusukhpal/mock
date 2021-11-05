9caa621a09aa69bdfd190d10e3e3f2ca37b59106
Himanshu Sukhpal <himanshu.sukhpal@gmail.com>
2021-11-05 23:45:50 +0530

    upgrade

---
+++ Social-ng-upgrade/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Social-ng-upgrade',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
